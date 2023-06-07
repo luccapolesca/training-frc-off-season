88202f21d31d65b1a02e57366d9de19dbffbb83e
Lucca Rezende Polesca <luccapolesca@gmail.com>
2023-06-06 21:59:36 -0700

    Une nouvelle robot

---
+++ src/main/java/frc/robot/Enemy.java
@@ -0,0 +1,15 @@
+package frc.robot;
+
+public class Enemy {
+    
+   public double x = 16;
+   public double y = 0;
+   public double xSpeed = -0.01;
+
+   public Enemy (double _x, double _y, double _xSpeed){
+
+      x = _x;
+      y = _y;
+      xSpeed = _xSpeed;
+   }
+}
