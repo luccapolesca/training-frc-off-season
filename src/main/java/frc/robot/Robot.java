84f27e7a57372dc285eb01ac2078e8ddbfe5bd27
Lucca Rezende Polesca <luccapolesca@gmail.com>
2023-06-11 21:55:38 -0700

    oui

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -29,36 +29,37 @@ public class Robot extends TimedRobot {
   MechanismLigament2d bras = null;
   MechanismLigament2d avantbras = null;
   MechanismLigament2d main = null;
-//Joystick
+  // Joystick
   Joystick key2 = null;
   Joystick key1 = null;
   Joystick keyD = null;
   Joystick key3 = null;
   Joystick keyA = null;
   Joystick key4 = null;
-//A
+  // A
   MechanismLigament2d t1 = null;
   MechanismLigament2d t2 = null;
   MechanismLigament2d t3 = null;
   MechanismLigament2d t4 = null;
-  
-//D
+
+  // D
   MechanismLigament2d Y1 = null;
   MechanismLigament2d Y2 = null;
   MechanismLigament2d Y3 = null;
 
-//Terrain 1
+  // Terrain 1
   Field2d field = null;
-  
+
   double xDeplacementPrecedent = 0;
   double yDeplacementPrecedent = 0;
   double degreePrecedent = 0;
   Random randy = new Random();
-  
+
   // Enemies
   Enemy[] enemyies = null;
-  // Enemy 2
-  // Enemy enemy2 = null;
+  // Des petits robots
+  SmallBot smallBots = null;
+
   /**
    * This function is run when the robot is first started up and should be used
    * for any
@@ -83,24 +84,21 @@ public class Robot extends TimedRobot {
     key1 = new Joystick(0);
     key3 = new Joystick(1);
 
-
     Mechanism2d tentacule = new Mechanism2d(20, 20);
 
-    //Tentacule gauche
+    // Tentacule gauche
     MechanismRoot2d root2 = tentacule.getRoot("TentaculesDeLucca", 5, 0);
     t1 = root2.append(new MechanismLigament2d("t1", 6, 90, 40, new Color8Bit(0, 150, 155)));
     t2 = t1.append(new MechanismLigament2d("t2", 4, 50, 27, new Color8Bit(10, 200, 100)));
     t3 = t2.append(new MechanismLigament2d("t3", 5, -100, 15, new Color8Bit(255, 128, 0)));
     t4 = t3.append(new MechanismLigament2d("t4", 2, 30, 20, new Color8Bit(51, 52, 225)));
-    
-    //Tentacule droit
+
+    // Tentacule droit
     MechanismRoot2d aeeee = tentacule.getRoot("aeeee", 15, 20);
     Y1 = aeeee.append(new MechanismLigament2d("Y1", 6, -90, 40, new Color8Bit(155, 155, 0)));
-    Y2 = Y1.append(new MechanismLigament2d("Y2", 4, 50, 27, new Color8Bit( 200, 100, 10)));
-    Y3 =Y2.append(new MechanismLigament2d("Y3", 5, -100, 15, new Color8Bit(0, 128, 255)));
-  
-    
-    
+    Y2 = Y1.append(new MechanismLigament2d("Y2", 4, 50, 27, new Color8Bit(200, 100, 10)));
+    Y3 = Y2.append(new MechanismLigament2d("Y3", 5, -100, 15, new Color8Bit(0, 128, 255)));
+
     // SmartDashboard.putData("Tentacules banboles", tentacule);
     SmartDashboard.putData("Mon Tentacules banboles", tentacule);
 
@@ -109,26 +107,30 @@ public class Robot extends TimedRobot {
     key3 = new Joystick(1);
     key4 = new Joystick(1);
 
-    //Creation de mon primiere terrain
+    // Creation de mon primiere terrain
     field = new Field2d();
     SmartDashboard.putData("terrain", field);
 
-    //Creer un robot enemy
+    // Creer un robot enemy
     enemyies = new Enemy[10];
-    for(int i = 0; i < enemyies.length; i++){
-    double yInitialEnemy = randy.nextDouble();
-    double ySpeedEnemy = -0.01 * randy.nextDouble() - 0.01;
-    enemyies[i] = new Enemy(16, yInitialEnemy * 8, ySpeedEnemy);
+    for (int i = 0; i < enemyies.length; i++) {
+      double yInitialEnemy = randy.nextDouble();
+      double ySpeedEnemy = -0.01 * randy.nextDouble() - 0.01;
+      enemyies[i] = new Enemy(16, yInitialEnemy * 8, ySpeedEnemy);
     }
-    // Creer un outre enemy
-
-    // enemy2 = new Enemy(16, yInitialEnemy * 8, -0.01);
+    // Creer les petits robots
+    smallBots = new SmallBot(xDeplacementPrecedent, degreePrecedent, kDefaultPeriod);
+    smallBots = null;
   }
 
-  /* (non-Javadoc)
+  /*
+   * (non-Javadoc)
+   * 
    * @see edu.wpi.first.wpilibj.IterativeRobotBase#robotPeriodic()
    */
-  /* (non-Javadoc)
+  /*
+   * (non-Javadoc)
+   * 
    * @see edu.wpi.first.wpilibj.IterativeRobotBase#robotPeriodic()
    */
   @Override
@@ -146,9 +148,7 @@ public class Robot extends TimedRobot {
     SmartDashboard.putNumber("mainAngle", mainAngle);
 
     avantbras.setAngle(avantbrasAngle);
-    main.setAngle(mainAngle); 
-
-    
+    main.setAngle(mainAngle);
 
     // Tentacule gauche
     double axisA = keyA.getRawAxis(0);
@@ -165,7 +165,7 @@ public class Robot extends TimedRobot {
     SmartDashboard.putNumber("tentacule3Angle", tentacule3Angle);
 
     t1.setAngle(tentaculeAngle);
-    t2.setAngle(tentacule1Angle); 
+    t2.setAngle(tentacule1Angle);
     t3.setAngle(tentacule2Angle);
     t4.setAngle(tentacule3Angle);
 
@@ -177,27 +177,28 @@ public class Robot extends TimedRobot {
 
     var tentaculyLonguer = 6 + 2 * axisD;
     var tentaculyAngle = 50 + 90 * axisW;
-    int red = (int)(127 + 127 * axis4);
+    int red = (int) (127 + 127 * axis4);
     Color8Bit tentaculyRed = new Color8Bit(red, 128, 255);
-   
+
     SmartDashboard.putNumber("tentaculyLongeur", tentaculyLonguer);
     SmartDashboard.putNumber("tentaculyAngle", tentaculyAngle);
- 
+
     Y1.setLength(tentaculyLonguer);
     Y2.setAngle(tentaculyAngle);
     Y3.setColor(tentaculyRed);
-    
-    /** Robot 1
-     * bouger le robot swerve et l'empecher de sortir du terrain 
-     */ 
+
+    /**
+     * Robot 1
+     * bouger le robot swerve et l'empecher de sortir du terrain
+     */
     boolean boutonRapide = keyA.getRawButton(1);
     SmartDashboard.putBoolean("boutonRapide", boutonRapide);
-    
-    double xDeplacement = keyA.getRawAxis(0)/10;
-    double yDeplacement = keyA.getRawAxis(1)/10;
+
+    double xDeplacement = keyA.getRawAxis(0) / 10;
+    double yDeplacement = keyA.getRawAxis(1) / 10;
     double degree = 45 * axis4;
 
-    if (boutonRapide){
+    if (boutonRapide) {
       xDeplacement = xDeplacement * 1.5;
       yDeplacement = yDeplacement * 1.5;
     }
@@ -208,60 +209,75 @@ public class Robot extends TimedRobot {
 
     SmartDashboard.putNumber("xterrain1", xDeplacement);
 
-    if (xDeplacement > 16){
+    if (xDeplacement > 16) {
       xDeplacement = 16;
     }
-    
-    if (xDeplacement < 0){
+
+    if (xDeplacement < 0) {
       xDeplacement = 0;
     }
-    
-    if (yDeplacement > 8){
+
+    if (yDeplacement > 8) {
       yDeplacement = 8;
     }
 
-    if (yDeplacement < 0){
+    if (yDeplacement < 0) {
       yDeplacement = 0;
     }
     xDeplacementPrecedent = xDeplacement;
     yDeplacementPrecedent = yDeplacement;
     degreePrecedent = degree;
 
-    field.getObject("robot").setPose(xDeplacement, yDeplacement, Rotation2d.fromDegrees(degree));
+    field.getObject("robot").setPose(0, yDeplacement, Rotation2d.fromDegrees(degree));
 
-    //Creer un robot enemy
-    for(int i = 0; i < enemyies.length; i++){
+    // Creer un robot enemy
+    for (int i = 0; i < enemyies.length; i++) {
       enemyies[i].x = enemyies[i].x + enemyies[i].xSpeed;
-     
-      if (enemyies[i].x <0){
+
+      if (enemyies[i].x < 0) {
         double yInitialEnemy = randy.nextDouble();
         enemyies[i] = new Enemy(16, yInitialEnemy * 8, -0.01);
         enemyies[i].x = 16;
       }
 
-      double xDistance = Math.abs(xDeplacement - enemyies[i].x);
-      boolean xSeTouche = xDistance < 0.5;
+      // double xDistance = Math.abs(smallBots.x - enemyies[i].x);
+      // boolean xSeTouche = xDistance < 0.5;
 
-      double yDistance = Math.abs(yDeplacement - enemyies[i].y);
-      boolean ySeTouche = yDistance < 0.5;
- 
-      if (xSeTouche && ySeTouche){
+      // double yDistance = Math.abs(smallBots.y - enemyies[i].y);
+      // boolean ySeTouche = yDistance < 0.5;
 
-        double yInitialEnemy = randy.nextDouble();
-        double ySpeedEnemy = -0.01 * randy.nextDouble() - 0.01;
- 
-        enemyies[i] = new Enemy(16, yInitialEnemy * 8, ySpeedEnemy);
-        enemyies[i].x = 16;
-      }
+      // if (xSeTouche && ySeTouche) {
+
+      //   double yInitialEnemy = randy.nextDouble();
+      //   double ySpeedEnemy = -0.01 * randy.nextDouble() - 0.01;
+
+      //   enemyies[i] = new Enemy(16, yInitialEnemy * 8, ySpeedEnemy);
+      //   enemyies[i].x = 16;
+      // }
 
       field.getObject("Enemy" + i).setPose(enemyies[i].x, enemyies[i].y, Rotation2d.fromDegrees(-180));
     }
 
+    // Des petites robots
+    boolean shootBullet = keyA.getRawButton(2);
+    SmartDashboard.putBoolean("petitRobot", shootBullet);
 
+    if (shootBullet){
+      smallBots = new SmallBot(xDeplacementPrecedent, degreePrecedent, kDefaultPeriod);
+    }
+    
+    if (smallBots != null){
+      smallBots.x = smallBots.x + smallBots.xSpeed * 2;
+
+      field.getObject("smallBots").setPose(smallBots.x, yDeplacement, Rotation2d.fromDegrees(degree));
+
+      
+      
+      if (smallBots.x > 16) {
+        smallBots = null;
+        field.getObject("smallBots").setPose(199, 0, Rotation2d.fromDegrees(degree));
+      }
+    }
   }
 
 }
-//Todo 
-// x du robot est tout jour 0 solmant possible de boujer a y
-// quand vous appuies sur un buton un petit robot est lance horizontalement
-// Si le petit robot touche au enemy le petit robot disparet et le l'enemy revient à droite (x = 16)
---
+++ src/main/java/frc/robot/SmallBot.java
@@ -0,0 +1,14 @@
+package frc.robot;
+
+public class SmallBot {
+    public double x = 0;
+   public double y = 0;
+   public double xSpeed = 1;
+
+   public SmallBot (double _x, double _y, double _xSpeed){
+
+      x = _x;
+      y = _y;
+      xSpeed = _xSpeed;
+   }
+}
